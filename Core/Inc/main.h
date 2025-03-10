3f533b763bb356348f57108a18fa2800d0053981
Marti Dominguez <marti98.md@gmail.com>
2025-03-10 20:42:15 +0100

    Add USART2 for CLI

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -28,6 +28,7 @@ extern "C" {
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx_hal.h"
+
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "C:\Development\Repos\faderbay\Apps\cli\cli.h"
