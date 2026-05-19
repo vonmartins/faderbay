90987f15bf4759a25556a5e814f4754ee084ed09
VonMartins <marti98.md@gmail.com>
2026-05-19 17:13:44 +0200

    Add display driver API declarations

--- Firmware/Faderbay/Drivers/display_driver.c
+++ Firmware/Faderbay/Drivers/display_driver.c
@@ -5,4 +5,12 @@
 
 #include "display_driver.h"
 
-/* TODO: implementation */
+fb_err_t DisplayDriver_Init(SPI_HandleTypeDef *hspi);
+
+void DisplayDriver_Clear(void);
+
+void DisplayDriver_DrawStr(uint8_t x, uint8_t y, const char *str);
+
+void DisplayDriver_DrawFrame(uint8_t x, uint8_t y, uint8_t w, uint8_t h);
+
+void DisplayDriver_Flush(void);
--- Firmware/Faderbay/Drivers/display_driver.h
+++ Firmware/Faderbay/Drivers/display_driver.h
@@ -1,8 +1,18 @@
 #ifndef DISPLAY_DRIVER_H
 #define DISPLAY_DRIVER_H
 
+#include "faderbay_types.h"
+#include "spi.h"
 #include <stdint.h>
 
-/* TODO: declarations */
+fb_err_t DisplayDriver_Init(SPI_HandleTypeDef *hspi);
+
+void DisplayDriver_Clear(void);
+
+void DisplayDriver_DrawStr(uint8_t x, uint8_t y, const char *str);
+
+void DisplayDriver_DrawFrame(uint8_t x, uint8_t y, uint8_t w, uint8_t h);
+
+void DisplayDriver_Flush(void);
 
 #endif /* DISPLAY_DRIVER_H */
